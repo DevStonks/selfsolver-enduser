5f80ff3c7ebc0a23f0fff03f1c98dbf4a53dd669
Capi Etheriel <barraponto@gmail.com>
2020-10-29 20:36:47 -0300

    automatically use jwt if token is available

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 import { selfsolverApi } from "../config";
+import TokenService from "./TokenService";
 
 const api = axios.create({
   baseURL: selfsolverApi,
 });
 
+api.interceptors.request.use(
+  (request) => {
+    if (TokenService.read()) {
+      request.headers.common.Authorization = `Bearer ${TokenService.get()}`;
+    }
+    return request;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default {
   login: (email, password) => api.post(`/login`, { email, password }),
+  getTickets: () => api.get("/tickets"),
 };
